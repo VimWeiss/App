10b470b0529b6dd752fdf5caac464a2cbb1d6b54
VimWeiss <goodgod03@gmail.com>
2024-02-22 15:53:46 +1000

    220224

--- django/blogging/iablog/views.py
+++ django/blogging/iablog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
 
 # Create your views here.
+
+class PostView(View):
+    #вывод записей
+    def get(self, request):
+        #request информация принятая от пользователя
+        posts = Post.objects.all()
+        return render(request, 'iablog/iablog.html', {'post_list': posts})
