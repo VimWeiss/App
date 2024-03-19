9d5d450b065b9dd4231c7dd7783a527de28da102
VimWeiss <goodgod03@gmail.com>
2024-03-19 15:54:21 +1000

    19032024

---
+++ Parsing/chgkparser.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'https://db.chgk.info/tour/disharm5_u'
+response = requests.get(url)
+response.raise_for_status()
+soup = BeautifulSoup(response.text, 'lxml')
+
+paragraph = soup.find_all(class_='question')
+
+file = open("parsing.txt", "w")
+
+for item in paragraph:
+    paragraph_text = item.text
+    file.write(paragraph_text)
+
+file.close()
